05ffa6a9a1605d97990b7fe7dbe879b06de493d2
Boros Bence <hello@bitben.hu>
2022-03-09 12:23:43 +0100

    Add project files.

---
+++ IskolaAPI/Models/diakok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace IskolaAPI.Models
+{
+    [Index(nameof(nev), Name = "nev", IsUnique = true)]
+    public partial class diakok
+    {
+        [Key]
+        [Column(TypeName = "int(11)")]
+        public int id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string nev { get; set; }
+        [Required]
+        [StringLength(22)]
+        public string email { get; set; }
+        [Column(TypeName = "smallint(1)")]
+        public short erdemjegy { get; set; }
+    }
+}

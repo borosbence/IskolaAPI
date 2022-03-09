df6fdb05a5854a57fd545d1d6f0b336e7ceb397a
Boros Bence <hello@bitben.hu>
2022-03-09 15:01:19 +0100

    átnevezés

--- Megoldás/IskolaAPI.Test/TestTanarokController.cs
+++ Megoldás/IskolaAPI.Test/TestTanarokController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace IskolaAPI.Test
 {
     [TestClass]
-    public class TestProductController
+    public class TestTanarokController
     {
         private TanarokController controller = new TanarokController(new iskolaContext());
         private tanarok GetPeldaTanar()
